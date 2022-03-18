4d5042b856c71569f6d041652ffdd1baddfe9d7b
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-03-18 12:22:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from '/three';
+import * as THREE from '/node_modules/three';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 65, window.innerWidth / window.innerHeight, 0.1, 2000 );
