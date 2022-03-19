ac6bd0cecfff3086efa808515d004a00db09f0da
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-03-20 00:19:03 +0200

    Update pageScroll.js

--- dist/pageScroll.js
+++ dist/pageScroll.js
@@ -1,45 +1,16 @@
+import { HemisphereLightProbe } from "three";
+import { SmoothShading } from "three";
 
 let home = document.getElementById("home");
 let skills = document.getElementById("about");
 let education = document.getElementById("education");
 let contact = document.getElementById("contact");
 
-window.addEventListener("scroll", function(){
-    var a = window.scrollY;
-    if(a > 0 && a < 600){
-        home.className = "homeJS";
-        skills.className = "about";
-        education.className = "education";
-        contact.className = "contact";
-    } if(a > 600 && a < 1700){
-        skills.className = "aboutJS";
-        home.className = "home";
-        education.className = "education";
-        contact.className = "contact";
-    } if(a > 1700 && a < 2800){
-        home.className = "home";
-        skills.className = "about";
-        education.className = "educationJS";
-        contact.className = "contact";
-    } if(a > 2800){
-        home.className = "home";
-        skills.className = "about";
-        education.className = "education";
-        contact.className = "contactJS";
-    }
-})
+home.addEventListener("click", scrollToElement(home));
+skills.addEventListener("click", scrollToElement(skills));
+education.addEventListener("click", scrollToElement(education));
+contact.addEventListener("click", scrollToElement(contact));
 
-home.onclick = function(){
-    window.scroll(0, 0);
-}
-skills.onclick = function(){
-    window.scroll(0, 1100);
-}
-
-education.onclick = function(){
-    window.scroll(0, 2200);
-}
-
-contact.onclick = function(){
-    window.scroll(0, 3300);
+function scrollToElement(el){
+    el.scrollIntoView({behavior: "smooth"});
 }
