a709208e835a066a43f8b651eafe95dbcb2b2c82
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-03-20 01:17:13 +0200

    Update pageScroll.js

--- dist/pageScroll.js
+++ dist/pageScroll.js
@@ -1,21 +1,45 @@
-var home = document.getElementById("home");
-var skills = document.getElementById("about");
-var education = document.getElementById("education");
-var contact = document.getElementById("contact");
 
-document.getElementById("home").onclick().scrollIntoView();
+let home = document.getElementById("home");
+let skills = document.getElementById("about");
+let education = document.getElementById("education");
+let contact = document.getElementById("contact");
 
-home.addEventListener("click", function(){
-    home.scrollIntoView();
-    console.log("click works");
-});
-skills.addEventListener("click", function(){
-    skills.scrollIntoView({behavior: "smooth"});
-});
-education.addEventListener("click", function(){
-    education.scrollIntoView({behavior: "smooth"});
-});
-contact.addEventListener("click", function(){
-    contact.scrollIntoView({behavior: "smooth"});
-});
+window.addEventListener("scroll", function(){
+    var a = window.scrollY;
+    if(a > 0 && a < 600){
+        home.className = "homeJS";
+        skills.className = "about";
+        education.className = "education";
+        contact.className = "contact";
+    } if(a > 600 && a < 1700){
+        skills.className = "aboutJS";
+        home.className = "home";
+        education.className = "education";
+        contact.className = "contact";
+    } if(a > 1700 && a < 2800){
+        home.className = "home";
+        skills.className = "about";
+        education.className = "educationJS";
+        contact.className = "contact";
+    } if(a > 2800){
+        home.className = "home";
+        skills.className = "about";
+        education.className = "education";
+        contact.className = "contactJS";
+    }
+})
 
+home.onclick = function(){
+    window.scroll(0, 0);
+}
+skills.onclick = function(){
+    window.scroll(0, 1100);
+}
+
+education.onclick = function(){
+    window.scroll(0, 2200);
+}
+
+contact.onclick = function(){
+    window.scroll(0, 3300);
+}
