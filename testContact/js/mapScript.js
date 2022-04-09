a0f96dd0dfe85bd1d9d7fec0b56d9fc779de055c
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-04-10 01:57:09 +0300

    niiiii

--- testContact/js/mapScript.js
+++ testContact/js/mapScript.js
@@ -3,7 +3,7 @@ function initMap() {
     // The map, centered at Uluru59.441020373451295, 24.731793031090394
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
-      center: uluru,
+      center: svetaBaar,
     });
     const marker = new google.maps.Marker({
       position: svetaBaar,
