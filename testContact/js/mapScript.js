3b3c8e08628362252f56dfd101abeb1712786b3e
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-04-10 01:28:46 +0300

    google api map

---
+++ testContact/js/mapScript.js
@@ -0,0 +1,12 @@
+function initMap() {
+    const svetaBaar = { lat: 59.441020373451295, lng: 24.731793031090394 };
+    // The map, centered at Uluru59.441020373451295, 24.731793031090394
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    const marker = new google.maps.Marker({
+      position: svetaBaar,
+      map: map,
+    });
+}
