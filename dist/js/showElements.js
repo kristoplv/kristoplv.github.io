65a522e3e4227119205013a4668a4ddccc0a88a2
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-07-16 11:20:36 +0300

    sd

--- dist/js/showElements.js
+++ dist/js/showElements.js
@@ -3,7 +3,7 @@ const aboutPicture = document.getElementById("ab-pic");
 
 var scrollToPosition = ()=>{
     var y = window.scrollY;
-    if(y >= 1200){
+    if(y >= 1100){
         aboutText.className = "about-text show-text";
         aboutPicture.className = "about-picture show-pic";
     } else {
