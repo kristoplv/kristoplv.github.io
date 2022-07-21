839f592b7fe91d4b537194592dd00252820c830a
Kristo Põlluvee <89929763+kristoplv@users.noreply.github.com>
2022-07-21 14:44:46 +0300

    sds

---
+++ dist/js/projectLinks.js
@@ -0,0 +1,18 @@
+var tois_link = document.querySelector("#tois");
+var chess_link = document.querySelector("#chess");
+var scraper_link = document.querySelector("#scraper");
+
+var java_link = document.querySelector("#java");
+
+tois_link.addEventListener("click", ()=>{
+    location.href = "https://github.com/kristoplv/kristoplv.github.io/tree/main/TLU_TOIS"
+})
+chess_link.addEventListener("click", ()=>{
+    location.href = "https://github.com/kristoplv/2-kodutoo-capture-the-king"
+})
+scraper_link.addEventListener("click", ()=>{
+    location.href = "https://github.com/kristoplv/kristoplv.github.io/tree/main/TLU_TOIS"
+})
+java_link.addEventListener("click", ()=>{
+    location.href = "https://github.com/kristoplv/oo2022"
+})
